84ab756b75c54e8a2d5496b4416515ca504be008
kuldeep singh <devkuldeeps89@gmail.com>
2021-12-14 19:26:13 +0530

    update

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1>Magic Match</h1>
+      <h1>Magic Match!</h1>
       <button>New Game</button>
     </div>
   );
